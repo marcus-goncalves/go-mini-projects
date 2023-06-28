2c6c62665805c49ab4c96d494b45e5142a325ad1
Marcus Gonçalves <nvrrox@gmail.com>
2023-06-27 23:06:20 -0300

    feat: integrate db calls

--- prj-fiber/internals/book/book.go
+++ prj-fiber/internals/book/book.go
@@ -1,35 +1,51 @@
 package book
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"projects.com/prj-fiber/internals/database"
+	"projects.com/prj-fiber/internals/entities"
+)
 
 func GetBooks(c *fiber.Ctx) error {
-	if err := c.SendString("All books"); err != nil {
-		c.SendString(err.Error())
-	}
+	db := database.DbConn
+	books := []entities.Book{}
 
-	return nil
+	db.Find(&books)
+	return c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) error {
-	if err := c.SendString("Single Book"); err != nil {
-		c.SendString(err.Error())
-	}
+	db := database.DbConn
+	id := c.Params("id")
+	book := entities.Book{}
 
-	return nil
+	db.Find(&book, id)
+	return c.JSON(book)
 }
 
 func NewBook(c *fiber.Ctx) error {
-	if err := c.SendString("New book"); err != nil {
-		c.SendString(err.Error())
+	db := database.DbConn
+	book := entities.Book{
+		Title:  "1984",
+		Author: "George Orwell",
+		Rating: 5,
 	}
 
-	return nil
+	db.Create(&book)
+	return c.JSON(book)
 }
 
 func DeleteBook(c *fiber.Ctx) error {
-	if err := c.SendString("delete book"); err != nil {
-		c.SendString(err.Error())
+	db := database.DbConn
+	id := c.Params("id")
+	book := entities.Book{}
+
+	db.First(&book, id)
+	if book.Title == "" {
+		c.Status(404).SendString("Book id not found")
+		return nil
 	}
 
-	return nil
+	db.Delete(&book)
+	return c.SendString("deleted")
 }
